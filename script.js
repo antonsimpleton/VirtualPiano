b97f28ca54d06ab09e8a310e740a3052d9fe09b8
antonsimpleton <antonsimpleton@yandex.ru>
2022-01-05 01:06:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+document.addEventListener("keydown", function(event) {
+    if (event.code == "KeyA") {
+        let soundA = new Audio("A.mp3");
+        soundA.play();
+    }
+    else if (event.code == "KeyS") {
+        let soundS = new Audio("S.mp3");
+        soundS.play();
+    }
+    else if (event.code == "KeyD") {
+        let soundD = new Audio("D.mp3");
+        soundD.play();
+    }
+    else if (event.code == "KeyF") {
+        let soundF = new Audio("F.mp3");
+        soundF.play();
+    }
+    else if (event.code == "KeyG") {
+        let soundG = new Audio("G.mp3");
+        soundG.play();
+    }
+    else if (event.code == "KeyH") {
+        let soundH = new Audio("H.mp3");
+        soundH.play();
+    }
+    else if (event.code == "KeyJ") {
+        let soundJ = new Audio("J.mp3");
+        soundJ.play();
+    }
+    else if (event.code == "KeyW") {
+        let soundW = new Audio("W.mp3");
+        soundW.play();
+    }
+    else if (event.code == "KeyE") {
+        let soundE = new Audio("E.mp3");
+        soundE.play();
+    }
+    else if (event.code == "KeyT") {
+        let soundT = new Audio("T.mp3");
+        soundT.play();
+    }
+    else if (event.code == "KeyY") {
+        let soundY = new Audio("Y.mp3");
+        soundY.play();
+    }
+    else if (event.code == "KeyU") {
+        let soundU = new Audio("U.mp3");
+        soundU.play();
+    }
+});
