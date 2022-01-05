8975c75eb00cc64fed5a60cd8d05eaae96e7d63c
antonsimpleton <antonsimpleton@yandex.ru>
2022-01-05 14:01:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,63 @@ document.addEventListener("keydown", function(event) {
         soundU.play();
     }
 });
+
+document.getElementById("A").addEventListener("click", function () {
+    let soundA = new Audio("A.mp3");
+    soundA.play();
+});
+
+document.getElementById("S").addEventListener("click", function () {
+    let soundS = new Audio("S.mp3");
+    soundS.play();
+});
+
+document.getElementById("D").addEventListener("click", function () {
+    let soundD = new Audio("D.mp3");
+    soundD.play();
+});
+
+document.getElementById("F").addEventListener("click", function () {
+    let soundF = new Audio("F.mp3");
+    soundF.play();
+});
+
+document.getElementById("G").addEventListener("click", function () {
+    let soundG = new Audio("G.mp3");
+    soundG.play();
+});
+
+document.getElementById("H").addEventListener("click", function () {
+    let soundH = new Audio("H.mp3");
+    soundH.play();
+});
+
+document.getElementById("J").addEventListener("click", function () {
+    let soundJ = new Audio("J.mp3");
+    soundJ.play();
+});
+
+document.getElementById("W").addEventListener("click", function () {
+    let soundW = new Audio("W.mp3");
+    soundW.play();
+});
+
+document.getElementById("E").addEventListener("click", function () {
+    let soundE = new Audio("E.mp3");
+    soundE.play();
+});
+
+document.getElementById("T").addEventListener("click", function () {
+    let soundT = new Audio("T.mp3");
+    soundT.play();
+});
+
+document.getElementById("Y").addEventListener("click", function () {
+    let soundY = new Audio("Y.mp3");
+    soundY.play();
+});
+
+document.getElementById("U").addEventListener("click", function () {
+    let soundU = new Audio("U.mp3");
+    soundU.play();
+});
